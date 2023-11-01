3527e36ebb44b97f4b0b5f10531eb1b10d9b9d56
pnevmat <pnevmat001@gmail.com>
2023-11-01 17:47:36 +0200

    fixes pin position bugs when drag to end and to start

--- src/components/ProductQuantitySlider/ProductQuantitySlider.js
+++ src/components/ProductQuantitySlider/ProductQuantitySlider.js
@@ -14,6 +14,7 @@ export default function ProductQuantitySlider({
   setValue,
 }) {
   const [pinStep, sePinStep] = useState(value);
+  const [pinWidth, setPinWidth] = useState(0);
   const [left, setLeft] = useState(value);
 
   const mainBlockRef = useRef(null);
@@ -22,27 +23,34 @@ export default function ProductQuantitySlider({
   useEffect(() => {
     if (mainBlockRef.current) {
       const mainBlockWidth = mainBlockRef.current.getBoundingClientRect().width;
+      const pinContainerWidth =
+        pinContainerRef.current.getBoundingClientRect().width;
 
       const actualMainBlockWidth = Math.ceil(mainBlockWidth);
+      const actualPinContainerWidth = Math.ceil(pinContainerWidth);
 
       if (actualMainBlockWidth !== 0) {
-        const pinStep = actualMainBlockWidth / maxValue;
+        const pinStep = (actualMainBlockWidth * step) / maxValue;
         console.log('Pin step: ', pinStep);
         sePinStep(pinStep);
+        setPinWidth(actualPinContainerWidth);
       }
     }
-  }, [maxValue]);
+  }, [maxValue, step]);
+
+  useEffect(() => {
+    if (value <= minValue || value === '2') setLeft(minValue);
+    if (value >= maxValue) setLeft(pinStep * maxValue - pinWidth);
+  }, [value, minValue, maxValue, pinStep, pinWidth]);
 
   const setValueHandler = (newValue) => {
-    if (newValue !== value && newValue !== minValue) {
+    if (newValue !== value && newValue > minValue) {
       setLeft((prevLeft) => {
         if (value < newValue) return prevLeft + pinStep;
         if (value > newValue) return prevLeft - pinStep;
       });
+      setValue(newValue);
     }
-
-    if (newValue === minValue) setLeft(minValue);
-    setValue(newValue);
   };
 
   console.log('Input range value: ', value);
