26d4fc062337141449adda2ecaae7283abd52b81
pnevmat <pnevmat001@gmail.com>
2023-11-04 20:10:49 +0200

    fixes bug with hovering and drugging pin svg

--- src/components/ProductQuantitySlider/ProductQuantitySlider.js
+++ src/components/ProductQuantitySlider/ProductQuantitySlider.js
@@ -14,10 +14,11 @@ export default function ProductQuantitySlider({
   setValue,
   style,
 }) {
-  // Todo: fix bug with unble to drag when hover on pin svg and continue to test when step is more than 1
-  const [pinStep, sePinStep] = useState(value);
+  // ToDo: test and fix calculation of products quantity with different step ammount
   const [pinWidth, setPinWidth] = useState(0);
-  const [left, setLeft] = useState(value);
+  const [pinStep, sePinStep] = useState(minValue);
+  const [sliderValue, setSliderValue] = useState(minValue);
+  const [left, setLeft] = useState(minValue);
 
   const mainBlockRef = useRef(null);
   const pinContainerRef = useRef(null);
@@ -33,7 +34,6 @@ export default function ProductQuantitySlider({
 
       if (actualMainBlockWidth !== 0) {
         const pinStep = actualMainBlockWidth / maxValue;
-        // console.log('Pin step: ', pinStep);
         sePinStep(pinStep);
         setPinWidth(actualPinContainerWidth);
       }
@@ -41,14 +41,15 @@ export default function ProductQuantitySlider({
   }, [maxValue, step]);
 
   useEffect(() => {
-    // || value === `${minValue + step}`
     if (value <= minValue) {
       setLeft(-pinWidth / 2 + 2);
+      setSliderValue(minValue);
       setValue(minValue);
     }
-    // || value >= `${maxValue - step}`
+
     if (value >= maxValue) {
       setLeft(pinStep * maxValue - pinWidth / 2 - 2);
+      setSliderValue(maxValue);
       setValue(maxValue);
     }
   }, [value, minValue, maxValue, step, pinStep, pinWidth, setValue]);
@@ -61,27 +62,28 @@ export default function ProductQuantitySlider({
         if (value < newValue || value > newValue)
           return newValue * pinStep - pinWidth / 2;
       });
-      setValue(newValue);
+      setSliderValue(newValue);
+      setValue(newValue * step);
     }
-
     if (newValue === `${minValue}`) {
       setLeft(-pinWidth / 2 + 2);
-      setValue(minValue);
+      setSliderValue(minValue);
+      setValue(minValue * step);
     }
-
     if (step > 1 && Number(newValue) + step > maxValue) {
       setLeft(pinStep * maxValue - pinWidth / 2 - 2);
-      setValue(maxValue);
+      setSliderValue(maxValue);
+      setValue(maxValue * step);
     }
   };
 
-  console.log('Input range value: ', value);
+  // console.log('Input range value: ', value);
   return (
     <>
       <div ref={mainBlockRef} className={s.container}>
         <div
           ref={pinContainerRef}
-          className={s.pinContainer}
+          className={`${s.pinContainer} 'background'`}
           style={{left: `${left}px`}}>
           {pin}
         </div>
@@ -89,10 +91,10 @@ export default function ProductQuantitySlider({
           className={s.inputRange}
           style={style ? style : {}}
           type="range"
-          value={value}
+          value={sliderValue}
           min={minValue}
           max={maxValue}
-          step={step}
+          step="1"
           onChange={(e) => setValueHandler(e.target.value)}
         />
       </div>
